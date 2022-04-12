68fab0c7791d7132c27d22a44c85075b53091775
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-12 10:48:31 -0400

    created a components folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import OrderedList from './OrderedList';
+import OrderedList from './components/OrderedList';
 function App() {
   return (
     <div className="App">
