47084ec66173125371f0394e66506005d95c62c2
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-13 08:32:18 -0400

    Prompt now returns the TextBox component and the Button component along witha div

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -2,5 +2,10 @@ import React, { component } from "react";
 import TextBox from "./TextBox";
 import Button from "./Button";
 function Prompt () {
-
+    return(
+        <div className="Prompt">
+        <TextBox></TextBox>
+        <Button className="Prompt"></Button>
+        </div>
+    )
 }
