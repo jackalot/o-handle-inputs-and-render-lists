b2139119aba12f97692cbfcecd0a52d52795a978
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-13 08:22:36 -0400

    added an if else statement that will return the button depending if it has a function or not

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,5 +6,16 @@ Function
 */
 function Button (props)
 {
-
+    if(props.myFunction)
+    {
+        return(
+            <button className={props.className} onClick={props.myFunction}></button>
+        ) 
+    }
+    else {
+        return(
+            <button className={props.className} onClick={console.log("I have no function. I'm a useless button!")}></button>
+        ) 
+    }
 }
+export default Button;
