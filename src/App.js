13a7391c5525a041feef3f3c6267982b8e6e8e02
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-13 08:37:21 -0400

    imported Prompt, added prompt as a component to render under OrderedList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import OrderedList from './components/OrderedList';
+import Prompt from './components/Prompt';
 function App() {
   return (
     <div className="App">
       <OrderedList></OrderedList>
-      
+      <Prompt></Prompt>
     </div>
   );
 }
